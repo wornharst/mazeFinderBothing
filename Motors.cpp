e264ae161ba1ce96fb684b7cfac1f585b339d85b
Krishnath-Sirivel <krishnath.sirivel@ontariotechu.net>
2026-02-27 12:02:28 -0500

    Add Motors.cpp with servo and motor definitions

---
+++ Motors.cpp
@@ -0,0 +1,10 @@
+#include <Servo.h>
+
+#define PWR_R 5               
+#define PWR_L 6               
+#define MTR_R 8               
+#define MTR_L 7               
+#define SERVO 10              
+#define MTR_ENABLE 3   
+         
+Servo scanServo;   
